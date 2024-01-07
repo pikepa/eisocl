4852f3176c2a7bf4ea85ba90221336473b6a9c56
Peter Pike <pikepeter@gmail.com>
2024-01-07 14:24:08 +1000

    formatting changes

--- app/Livewire/Notifications/ShowUserNotifications.php
+++ app/Livewire/Notifications/ShowUserNotifications.php
@@ -21,7 +21,7 @@ class ShowUserNotifications extends Component
         return <<<'HTML'
         <div >
             @if($notifications->count() > 0)
-                <x-dropdown align="right" width="48">
+                <x-dropdown align="right" width="60">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-blue-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <span> <i class="fa-solid fa-bell"></i> </span>
@@ -30,7 +30,9 @@ class ShowUserNotifications extends Component
 
                     <x-slot name="content">
                         @foreach($notifications as $notification)
-                            <x-dropdown-link  href=" {{ $notification->data['link'] }} ">
+                            <x-dropdown-link 
+                                wire:click="markAsRead('{{ $notification->id  }}')" 
+                                href=" {{ $notification->data['link'] }} ">
                                 {{ $notification->data['message'] }}
                             </x-dropdown-link>
                         @endforeach
